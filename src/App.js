f299791b843183c89f9995d0f4ce9bab161e24d8
shadowgamer1216 <meamill88@gmail.com>
2022-11-04 11:15:18 -0700

    Contact page
    Fleshing out contact page and added to homepage

--- src/App.js
+++ src/App.js
@@ -9,13 +9,15 @@ import {
 
 import HomePage from './HomePage';
 import ProductPage from './product';
+import ContactPage from './contact';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path = '/' element = {<HomePage/>} />
-        <Route path = "product" element = {<ProductPage/>} />      
+        <Route path = "product" element = {<ProductPage/>} />    
+        <Route path = "contact" element = {<ContactPage/>} /> 
       </Routes>
     
     </div>
