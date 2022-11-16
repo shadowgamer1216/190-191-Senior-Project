3b930353d519d6b86cd0396d9b802ecc155879fb
shadowgamer1216 <meamill88@gmail.com>
2022-11-16 13:00:24 -0800

    Homepage edits
    Messing around with homepage

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,35 +1,94 @@
-import logo from './absolutemedialogo.png';
+/*import logo from './absolutemedialogo.png';*/
 import React from 'react';
+import './App.css';
 
 import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
-    <div>
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>
-        Home page
-        </h1> 
-        <Link to = "Company">+ Add Company</Link>
-        <br></br>
-        <Link to = "Components">+ Add Component</Link>
-        <br></br>
-        <Link to = "Contact">+ Add Contact</Link>
-        <br></br>
-        <Link to = "Location">+ Add Location</Link>
-        <br></br>
-        <Link to = "Order">+ Add Order</Link>
-        <br></br>
-        <Link to = "Shipping">+ Add Shipping</Link>
-        <br></br>
-        <Link to = "Product">+ Add Product</Link>
-        <br></br>
-        <Link to = "ItemCheckIn">+ Item Check In</Link>
-        <br></br>
-        <Link to = "View">+ View</Link>
-        <br></br>
-        <Link to = "Edit">+ Edit</Link>
-        <br></br>
+    
+    /*<div>*/
+    <div class='HomePage'>
+      {/*<img src={logo} className="App-logo" alt="logo" />*/}
+      <div class='HomePageBar'>
+        <h1>
+          Absolute Media, Inc
+        </h1>
+      </div>
+        <div class='testblock1'>
+        <Link to = "Company">
+          <div className="Add Company">
+          <button type="button" id="Add Company" class="HomePageButton">+ Add Company</button>
+          <Link to = "Shipping">
+          <button type="button" id="Add Shipping" class="HomePageButton">+ Add Shipping</button>
+          </Link>
+          </div>
+        </Link>
+        </div>
+
+        <Link to = "Components">
+        <div className="Add Component">
+            <button type="Add Component" id="Add Component" class="HomePageButton">Add Component</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
+        
+
+        <Link to = "Contact">
+        <div className="Add Contact">
+            <button type="Add Contact" id="Add Contact" class="HomePageButton">Add Contact</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
+
+        <Link to = "Location">
+        <div className="Add Location">
+            <button type="Add Location" id="Add Location" class="HomePageButton">Add Location</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
+
+        <Link to = "Order">
+        <div className="Add Order">
+            <button type="Add Order" id="Add Order" class="HomePageButton">Add Order</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
+
+        <Link to = "Shipping">
+        <div className="Add Shipping">
+            <button type="Add Shipping" id="Add Shipping" class="HomePageButton">Add Shipping</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
+
+        <Link to = "Product">
+        <div className="Add Product">
+            <button type="Add Product" id="Add Product" class="HomePageButton">Add Product</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
+
+        <Link to = "ItemCheckIn">
+        <div className="Item Check In">
+            <button type="Item Check In" id="Item Check In" class="HomePageButton">Item Check In</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
+
+        <Link to = "View">
+        <div className="View">
+            <button type="View" id="View" class="HomePageButton">View</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
+
+        <Link to = "Edit">
+        <div className="Edit">
+            <button type="Edit" id="Edit" class="HomePageButton">Edit</button>
+          </div>
+        </Link>
+        {/*<br></br>*/}
     </div>
   );
 }
