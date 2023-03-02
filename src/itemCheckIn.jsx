2a968c06f9b956bfe5f76d841e38077e17deafd8
Kyler3013 <kylerng1@gmail.com>
2023-03-01 16:58:42 -0800

    started connecting item check in page to backend HB8-87

--- src/itemCheckIn.jsx
+++ src/itemCheckIn.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css'
 import { Link, useNavigate } from "react-router-dom"
+import Axios from "axios";
 
 const ItemCheckIn = () => {
     const navigate = useNavigate();
+    const [customer_id, setCustomerID] = useState("");
+    const [item_id, setItemID] = useState("");
+    const [mfr_pn, setMfrPn] = useState("");
+    const [description, setDescription] = useState("");
+    const [carrier, setCarrier] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [disposition, setDisposition] = useState("");
+    const [signed_for_by, setSignedForBy] = useState("");
+    const [date_in, setDateIn] = useState("");
+    const [date_complete, setDateComplete] = useState("");
+
+    const submit = () => {
+        Axios.post("http://localhost:3001/api/insert", {customer_id: customer_id, item_id: item_id, mfr_pn: mfr_pn, description: description, carrier: carrier, quantity: quantity, disposition: disposition, signed_for_by: signed_for_by, date_in: date_in, date_complete: date_complete})
+        .then(()=> {
+            alert('inserted');
+        })
+    };
+
     return (
         <div className="page">
             <nav className="navbar navbar-expand-lg navbar-dark bg-maroon">
@@ -37,49 +56,63 @@ const ItemCheckIn = () => {
                         <div className="form-row">
                             <label htmlFor="customer-id" className="col-sm-2 col-form-label">Customer ID</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="customer-id" />
+                                <input type="text" className="form-control" id="customer-id" onChange={(e) =>{
+                                    setCustomerID(e.target.value)
+                                }}/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="item-id" className="col-sm-2 col-form-label">Item ID</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="item-id" />
+                                <input type="text" className="form-control" id="item-id" onChange={(e) =>{
+                                    setItemID(e.target.value)
+                                }}/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="mfr-pn" className="col-sm-2 col-form-label">MFR PN</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="mfr-pn" />
+                                <input type="text" className="form-control" id="mfr-pn" onChange={(e) =>{
+                                    setMfrPn(e.target.value)
+                                }}/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="description" className="col-sm-2 col-form-label">Description</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="description" />
+                                <input type="text" className="form-control" id="description" onChange={(e) =>{
+                                    setDescription(e.target.value)
+                                }}/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="carrier" className="col-sm-2 col-form-label">Carrier</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="carrier" />
+                                <input type="text" className="form-control" id="carrier" onChange={(e) =>{
+                                    setCarrier(e.target.value)
+                                }}/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="quantity" className="col-sm-2 col-form-label">Quantity</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="quantity" />
+                                <input type="text" className="form-control" id="quantity" onChange={(e) =>{
+                                    setQuantity(e.target.value)
+                                }}/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="disposition" className="col-sm-2 col-form-label">Disposition</label>
                             <div className="input-group input-group-sm mb-3 col-sm-3">
-                                <select className="form-control" name="disposition" id="disposition">
+                                <select className="form-control" name="disposition" id="disposition" onChange={(e) =>{
+                                    setDisposition(e.target.value)
+                                }}>
                                     <option selected value="">Select Value</option>
                                     <option value="1">X1_TBD</option>
                                     <option value="2">X2_TBD</option>
@@ -91,21 +124,27 @@ const ItemCheckIn = () => {
                         <div className="form-row">
                             <label htmlFor="signed-for-by" className="col-sm-2 col-form-label">Signed For By</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="signed-for-by" />
+                                <input type="text" className="form-control" id="signed-for-by" onChange={(e) =>{
+                                    setSignedForBy(e.target.value)
+                                }}/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="date-in" className="col-sm-2 col-form-label">Date In</label>
                             <div className="input-group input-group-sm mb-3 col-sm-3">
-                                <input type="date" className="form-control" name="date-in" id="date-in" />
+                                <input type="date" className="form-control" name="date-in" id="date-in" onChange={(e) =>{
+                                    setDateIn(e.target.value)
+                                }}/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="date-completed" className="col-sm-2 col-form-label">Date Completed</label>
                             <div className="input-group input-group-sm mb-3 col-sm-3">
-                                <input type="date" className="form-control" name="date-completed" id="date-completed" />
+                                <input type="date" className="form-control" name="date-completed" id="date-completed" onChange={(e) =>{
+                                    setDateComplete(e.target.value)
+                                }}/>
                             </div>
                         </div>
                     </div>
