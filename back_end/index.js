4763240655a6854c869f19cfaa48bd062cc7701f
Mark Chavez <markchavez@Marks-MacBook-Pro-4.local>
2023-03-12 20:38:02 -0700

    invoice stuff

--- back_end/index.js
+++ back_end/index.js
@@ -4,6 +4,7 @@ const app = express();
 const mysql = require("mysql2");
 const cors = require("cors");
 
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
--- src/invoice.jsx
+++ src/invoice.jsx
@@ -1,69 +1,37 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-const { Sequelize } = require ('sequelize');
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Invoice({ match }) {
-  const sequelize = new Sequelize(
-    'absolutemedia',
-    'root',
-    'password', 
-    {
-        host: 'localhost',
-        dialect: 'mysql'
-    }
-  );
-  var order = sequelize.define('Order')
+  const [invoiceData, setInvoiceData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Use the `match.params.orderId` variable to construct the URL with the order ID
-    axios.get(`http://localhost:3002/api/orders/${match.params.orderId}`)
-      .then((response) => {
-        setOrder(response.data);
-      })
-      .catch((error) => {
+    async function fetchInvoiceData() {
+      try {
+        const response = await axios.get(`/api/orders/${match.params.id}`);
+        setInvoiceData(response.data);
+        setLoading(false);
+      } catch (error) {
         console.error(error);
-      });
-  }, [match.params.orderId]);
+      }
+    }
 
+    fetchInvoiceData();
+  }, [match.params.id]);
 
-  if (!order) {
+  if (loading) {
     return <div>Loading...</div>;
   }
 
-  const {
-    company,
-    companyAddress,
-    invoiceNumber,
-    customerName,
-    customerAddress,
-    invoiceDate,
-    items,
-    total,
-  } = order;
-
   return (
-    <div className="invoice">
-      <div className="header">
-        <div className="company">
-          <h1>{company}</h1>
-          <p>{companyAddress}</p>
-        </div>
-        <div className="invoice-number">
-          <h2>Invoice #{invoiceNumber}</h2>
-        </div>
-      </div>
-      <div className="billing">
-        <div className="customer">
-          <h3>Bill To:</h3>
-          <p>{customerName}</p>
-          <p>{customerAddress}</p>
-        </div>
-        <div className="invoice-date">
-          <h3>Invoice Date:</h3>
-          <p>{invoiceDate}</p>
-        </div>
-      </div>
-      <table className="items">
+    <div>
+      <h1>Invoice #{invoiceData.invoiceNumber}</h1>
+      <p>Company: {invoiceData.company}</p>
+      <p>Company Address: {invoiceData.companyAddress}</p>
+      <p>Customer: {invoiceData.customerName}</p>
+      <p>Customer Address: {invoiceData.customerAddress}</p>
+      <p>Invoice Date: {invoiceData.invoiceDate}</p>
+      <table>
         <thead>
           <tr>
             <th>Quantity</th>
@@ -73,22 +41,17 @@ function Invoice({ match }) {
           </tr>
         </thead>
         <tbody>
-          {items.map((item) => (
+          {invoiceData.items.map(item => (
             <tr key={item.id}>
               <td>{item.quantity}</td>
               <td>{item.description}</td>
-              <td>${item.unitPrice.toFixed(2)}</td>
-              <td>${item.totalPrice.toFixed(2)}</td>
+              <td>{item.unitPrice}</td>
+              <td>{item.totalPrice}</td>
             </tr>
           ))}
         </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan="3">Total:</td>
-            <td>${total.toFixed(2)}</td>
-          </tr>
-        </tfoot>
       </table>
+      <p>Total: {invoiceData.total}</p>
     </div>
   );
 }
