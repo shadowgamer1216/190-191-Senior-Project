4e78968d03dde84f37d63982665da088a4dcba6f
Jared Roque <91704454+Jroque1011@users.noreply.github.com>
2023-03-29 15:47:24 -0700

    Recomitting Company.jsx if any issues

--- src/company.jsx
+++ src/company.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css'
 import { Link, useNavigate } from "react-router-dom"
+import Axios from "axios";
 
 const Company = () => {
     const navigate = useNavigate();
+    const [company_ID, setCompanyID] = useState(null);
+    const [company_Name, setCompanyName] = useState(null);
+    const [addr1, setAddr1] = useState(null);
+    const [addr2, setAddr2] = useState(null);
+    const [city, setCity] = useState(null);
+    const [state, setState] = useState(null);
+    const [country, setCountry] = useState(null);
+    const [zip, setZip] = useState(null);
+    const [Salesperson, setSalesperson] = useState(null);
+    const [phone, setPhone] = useState(null);
+    const [Extension, setExtension] = useState(null);
+    const [fax, setFax] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [Web_addr, setWebAddr] = useState(null);
+    const [Tax_ID, setTaxID] = useState(null);
+    const [credit_checked, setCreditChecked] = useState(null);
+    const [status, setStatus] = useState(null);
+    const [customer, setCustomer] = useState(null);
+    const [vendor, setVendor] = useState(null);
+    const [OEM, setOEM] = useState(null);
+    const [other_class, setOtherClass] = useState(null);
+    const [notes, setNotes] = useState(null);
+    
+    const submit = () => {
+        Axios.post("http://localhost:3001/api/insertCompany", {company_ID: company_ID, company_Name: company_Name, addr1: addr1, addr2: addr2, city: city, state: state, country: country, zip: zip, Salesperson: Salesperson, phone: phone, Extension: Extension, fax: fax, email: email, Web_addr: Web_addr, Tax_ID: Tax_ID, credit_checked: credit_checked, status: status, customer: customer, vendor: vendor, OEM: OEM, other_class: other_class, notes: notes})
+        .then(()=> {
+            alert('inserted Company');
+        })
+    };
+
     return (
         <div className="page">
             <nav className="navbar navbar-expand-lg navbar-dark bg-maroon">
@@ -39,14 +70,18 @@ const Company = () => {
                         <div className="form-row">
                             <label htmlFor="CID" className="col-sm-2 col-form-label">Company ID</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="CID" />
+                                <input type="text" className="form-control" id="CID" onChange={(e) =>{
+                                    setCompanyID(e.target.value)
+                                }} maxLength = "8" required/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="CName" className="col-sm-2 col-form-label">Company Name</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="CName" />
+                                <input type="text" className="form-control" id="CName" onChange={(e) =>{
+                                    setCompanyName(e.target.value)
+                                }} maxLength = "128"/>
                             </div>
                         </div>
                     </div>
@@ -60,28 +95,36 @@ const Company = () => {
                         <div className="form-row">
                             <label htmlFor="Addr1" className="col-sm-2 col-form-label">Address 1</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="Addr1" />
+                                <input type="text" className="form-control" id="Addr1" onChange={(e) =>{
+                                    setAddr1(e.target.value)
+                                }} maxLength = "64"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="Addr2" className="col-sm-2 col-form-label">Address 2</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="Addr2" />
+                                <input type="text" className="form-control" id="Addr2" onChange={(e) =>{
+                                    setAddr2(e.target.value)
+                                }} maxLength = "64"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="city" className="col-sm-2 col-form-label">City</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="city" />
+                                <input type="text" className="form-control" id="city" onChange={(e) =>{
+                                    setCity(e.target.value)
+                                }} maxLength = "64"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="state" className="col-sm-2 col-form-label">State</label>
                             <div className="input-group input-group-sm mb-3 col-sm-3">
-                                <select className="form-control" name="state" id="state">
+                                <select className="form-control" name="state" id="state" onChange={(e) =>{
+                                    setState(e.target.value)
+                                }}>
                                     <option selected value="">Select State</option>
                                     <option value="AL">AL</option>
                                     <option value="AK">AK</option>
@@ -141,7 +184,9 @@ const Company = () => {
                         <div className="form-row">
                             <label htmlFor="zip" className="col-sm-2 col-form-label">Zip</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="zip" />
+                                <input type="text" className="form-control" id="zip" onChange={(e) =>{
+                                    setZip(e.target.value)
+                                }} maxLength = "16"/>
                             </div>
                         </div>
 
@@ -149,7 +194,9 @@ const Company = () => {
                         <div className="form-row">
                             <label htmlFor="country" className="col-sm-2 col-form-label">Country</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="country" />
+                                <input type="text" className="form-control" id="country" onChange={(e) =>{
+                                    setCountry(e.target.value)
+                                }} maxLength = "25"/>
                             </div>
                         </div>
 
@@ -164,42 +211,54 @@ const Company = () => {
                         <div className="form-row">
                             <label htmlFor="sp" className="col-sm-2 col-form-label">Salesperson</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="sp" />
+                                <input type="text" className="form-control" id="sp" onChange={(e) =>{
+                                    setSalesperson(e.target.value)
+                                }} maxLength = "60"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="phone" className="col-sm-2 col-form-label">Phone</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="phone" />
+                                <input type="text" className="form-control" id="phone" onChange={(e) =>{
+                                    setPhone(e.target.value)
+                                }} maxLength = "16"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="ext" className="col-sm-2 col-form-label">Extension</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="ext" />
+                                <input type="text" className="form-control" id="ext" onChange={(e) =>{
+                                    setExtension(e.target.value)
+                                }} maxLength = "5"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="fax" className="col-sm-2 col-form-label">Fax</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="fax" />
+                                <input type="text" className="form-control" id="fax" onChange={(e) =>{
+                                    setFax(e.target.value)
+                                }} maxLength = "16"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="email" />
+                                <input type="text" className="form-control" id="email" onChange={(e) =>{
+                                    setEmail(e.target.value)
+                                }} maxLength = "128"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="wAddr" className="col-sm-2 col-form-label">WebAddress</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="wAddr" />
+                                <input type="text" className="form-control" id="wAddr" onChange={(e) =>{
+                                    setWebAddr(e.target.value)
+                                }} maxLength = "128"/>
                             </div>
                         </div>
                     </div>
@@ -212,14 +271,17 @@ const Company = () => {
                         <div className="form-row">
                             <label htmlFor="TID" className="col-sm-2 col-form-label">Tax ID</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="TID" />
+                                <input type="text" className="form-control" id="TID" onChange={(e) =>{
+                                    setTaxID(e.target.value)
+                                }} maxLength = "64"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="Status" className="col-sm-2 col-form-label">Status</label>
                             <div className="input-group input-group-sm mb-3 col-sm-3">
-                                <select className="form-control" name="Status" id="Status">
+                                <select className="form-control" name="Status" id="Status" onChange={(e) =>{
+                                    setStatus(e.target.value)}}>
                                     <option defaultValue="act">Active</option>
                                     <option value="inAct">Inactive</option>
                                 </select>
@@ -229,7 +291,8 @@ const Company = () => {
                         <div className="form-row">
                             <div className="input-group input-group-sm col-sm-3 pl-5">
                                 <div className="form-group custom-control custom-checkbox">
-                                    <input type="checkbox" className="custom-control-input" id="creditCheck" />
+                                    <input type="checkbox" className="custom-control-input" id="creditCheck" onChange={(e) =>{
+                                    setCreditChecked(e.target.value)}}/>
                                     <label htmlFor="creditCheck" className="custom-control-label">Credit Checked?</label>
                                 </div>
                             </div>
@@ -237,21 +300,24 @@ const Company = () => {
 
                             <div className="input-group input-group-sm col-sm-3 pl-5">
                                 <div className="form-group custom-control custom-checkbox">
-                                    <input type="checkbox" className="custom-control-input" id="customerCheck" />
+                                    <input type="checkbox" className="custom-control-input" id="customerCheck" onChange={(e) =>{
+                                    setCustomer(e.target.value)}}/>
                                     <label htmlFor="customerCheck" className="custom-control-label">Customer?</label>
                                 </div>
                             </div>
 
                             <div className="input-group input-group-sm col-sm-3 pl-5">
                                 <div className="form-group custom-control custom-checkbox">
-                                    <input type="checkbox" className="custom-control-input" id="vendorCheck" />
+                                    <input type="checkbox" className="custom-control-input" id="vendorCheck" onChange={(e) =>{
+                                    setVendor(e.target.value)}}/>
                                     <label htmlFor="vendorCheck" className="custom-control-label">Vendor?</label>
                                 </div>
                             </div>
 
                             <div className="input-group input-group-sm col-sm-3 pl-5">
                                 <div className="form-group custom-control custom-checkbox">
-                                    <input type="checkbox" className="custom-control-input" id="OEMCheck" />
+                                    <input type="checkbox" className="custom-control-input" id="OEMCheck" onChange={(e) =>{
+                                    setOEM(e.target.value)}}/>
                                     <label htmlFor="OEMCheck" className="custom-control-label">OEM?</label>
                                 </div>
                             </div>
@@ -260,22 +326,23 @@ const Company = () => {
                         <div className="form-row">
                             <label htmlFor="otherClass" className="col-sm-2 col-form-label">Other Classification</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <input type="text" className="form-control" id="otherClass" />
+                                <input type="text" className="form-control" id="otherClass" onChange={(e) =>{
+                                    setOtherClass(e.target.value)
+                                }} maxLength = "64"/>
                             </div>
                         </div>
 
                         <div className="form-row">
                             <label htmlFor="notes" className="col-sm-2 col-form-label">Notes</label>
                             <div className="input-group input-group-sm mb-3 col-sm-10">
-                                <textarea rows="4" cols="50" className="form-control" name="notes" id="custom-area" />
+                                <textarea rows="4" cols="50" className="form-control" name="notes" id="custom-area" onChange={(e) =>{
+                                    setNotes(e.target.value)}}/>
                             </div>
                         </div>
                     </div>
 
                     <div className="Add/Cancel">
-                        <button type="Add" id="add-company" className="btn btn-success m-2">Add</button>
-                        <button type="Cancel" id="Cancel-company" className="btn btn-success m-2">Cancel</button>
-
+                        <button onClick = {submit} type="Add" id="add-company" className="btn btn-success m-2">Add</button>
                     </div>
                 </form>
 
@@ -304,5 +371,3 @@ const Company = () => {
     //status might have something else in the dropbox, it is not shown
 }
 export default Company;
-
-
